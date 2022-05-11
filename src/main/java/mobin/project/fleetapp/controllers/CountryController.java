880a9bedb0ff05868f8e7114e02bb136eddcfc62
mobin-newroz <ahmobin@newroztech.com>
2022-05-11 18:01:04 +0600

    countries list display and store new country

--- src/main/java/mobin/project/fleetapp/controllers/CountryController.java
+++ src/main/java/mobin/project/fleetapp/controllers/CountryController.java
@@ -1,13 +1,31 @@
 package mobin.project.fleetapp.controllers;
 
+import mobin.project.fleetapp.models.Country;
+import mobin.project.fleetapp.services.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
 
+    @Autowired
+    CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries(){
+    public String index(Model model){
+        List<Country> countryList = countryService.getCountries();
+        model.addAttribute("countries",countryList);
         return "country";
+    }
+
+    @PostMapping("/countries")
+    public String store(Country country) {
+        countryService.store(country);
+        return "redirect:/countries";
     }
 }
--- src/main/java/mobin/project/fleetapp/services/CountryService.java
+++ src/main/java/mobin/project/fleetapp/services/CountryService.java
@@ -1,7 +1,25 @@
 package mobin.project.fleetapp.services;
 
+import mobin.project.fleetapp.models.Country;
+import mobin.project.fleetapp.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    CountryRepository countryRepository;
+
+    //list of countries
+    public List<Country> getCountries(){
+        return countryRepository.findAll();
+    }
+
+    //store new country
+    public void store(Country country){
+        countryRepository.save(country);
+    }
 }
