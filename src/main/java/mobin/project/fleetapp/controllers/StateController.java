b5ef02f7a802f726d4214ca1af1db15f2d857ae4
mobin-newroz <ahmobin@newroztech.com>
2022-05-16 19:10:56 +0600

    state module create

--- src/main/java/mobin/project/fleetapp/controllers/StateController.java
+++ src/main/java/mobin/project/fleetapp/controllers/StateController.java
@@ -1,13 +1,48 @@
 package mobin.project.fleetapp.controllers;
 
+import mobin.project.fleetapp.models.Country;
+import mobin.project.fleetapp.models.State;
+import mobin.project.fleetapp.services.CountryService;
+import mobin.project.fleetapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StateController {
 
+    @Autowired
+    StateService stateService;
+    @Autowired
+    CountryService countryService;
+
     @GetMapping("/states")
-    public String getCountries(){
+    public String getStates(Model model){
+        model.addAttribute("page","states");
+        List<State> states = stateService.getStates();
+        List<Country> countries = countryService.getCountries();
+        model.addAttribute("stateList",states);
+        model.addAttribute("countries",countries);
         return "states";
+    }
+
+    @PostMapping( "states")
+    public String storeState(State state){
+        stateService.storeOrUpdate(state);
+        return "redirect:/states";
+    }
+
+
+    @RequestMapping("states/show")
+    @ResponseBody
+    public Optional<State> getSingleState(int id){
+        return stateService.findById(id);
     }
 }
--- src/main/java/mobin/project/fleetapp/services/StateService.java
+++ src/main/java/mobin/project/fleetapp/services/StateService.java
@@ -1,7 +1,27 @@
 package mobin.project.fleetapp.services;
 
+import mobin.project.fleetapp.models.State;
+import mobin.project.fleetapp.repositories.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StateService {
+    @Autowired
+    StateRepository stateRepository;
+
+    public List<State> getStates(){
+        return stateRepository.findAll();
+    }
+
+    public void storeOrUpdate(State state){
+        stateRepository.save(state);
+    }
+
+    public Optional<State> findById(int id){
+        return stateRepository.findById(id);
+    }
 }
