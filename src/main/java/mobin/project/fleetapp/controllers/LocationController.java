6521ae394380db97e93b69de88f60be57c552d44
mobin-newroz <ahmobin@newroztech.com>
2022-05-17 16:01:02 +0600

    location crud - read and create

--- src/main/java/mobin/project/fleetapp/controllers/LocationController.java
+++ src/main/java/mobin/project/fleetapp/controllers/LocationController.java
@@ -1,13 +1,41 @@
 package mobin.project.fleetapp.controllers;
 
+import mobin.project.fleetapp.models.Country;
+import mobin.project.fleetapp.models.Location;
+import mobin.project.fleetapp.models.State;
+import mobin.project.fleetapp.services.CountryService;
+import mobin.project.fleetapp.services.LocationService;
+import mobin.project.fleetapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class LocationController {
 
+    @Autowired LocationService locationService;
+    @Autowired CountryService countryService;
+    @Autowired StateService stateService;
+
     @GetMapping("/locations")
-    public String getCountries(){
+    public String getCountries(Model model){
+        model.addAttribute("page","locations");
+        List<Location> locationList = locationService.locationList();
+        List<Country> countryList = countryService.getCountries();
+        List<State> stateList = stateService.getStates();
+        model.addAttribute("locations",locationList);
+        model.addAttribute("countries",countryList);
+        model.addAttribute("states",stateList);
         return "locations";
+    }
+
+    @PostMapping("/locations")
+    public String storeLocation(Location location){
+        locationService.storeOrUpdateLocation(location);
+        return "redirect:/locations";
     }
 }
--- src/main/java/mobin/project/fleetapp/services/LocationService.java
+++ src/main/java/mobin/project/fleetapp/services/LocationService.java
@@ -1,7 +1,36 @@
 package mobin.project.fleetapp.services;
 
+import mobin.project.fleetapp.models.Location;
+import mobin.project.fleetapp.repositories.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LocationService {
+    @Autowired
+    LocationRepository locationRepository;
+
+    public List<Location> locationList(){
+        return locationRepository.findAll();
+    }
+
+
+    public Optional<Location> getLocationById(int id){
+        return locationRepository.findById(id);
+    }
+
+    public Location getLocation(int id){
+        return locationRepository.findById(id).get();
+    }
+
+    public void storeOrUpdateLocation(Location location){
+        locationRepository.save(location);
+    }
+
+    public void destroyLocationById(int id){
+        locationRepository.deleteById(id);
+    }
 }
